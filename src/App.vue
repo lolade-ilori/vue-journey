a0502b2928e19a168342a34c5ad21f29384d1377
Lolade Ilori <loladeilori02@gmail.com>
2025-02-12 13:46:03 +0000

    adding and deleting tasks

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ const name = ref("John Doe")
 const count = ref(0)
 const status = ref('active')
 const tasks = ref(['Task 1', 'Task3', 'Task 0'])
+const newTask = ref('')
 
 const increment = () => {
   count.value++
@@ -20,6 +21,17 @@ const toggleStatus = () => {
     }
 }
 
+const addTask = () => {
+  if(newTask.value.trim() !== '') {
+    tasks.value.push(newTask.value)
+    newTask.value = ''
+  }
+}
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1)
+}
+
 </script>
 
 
@@ -32,14 +44,21 @@ const toggleStatus = () => {
   <br />
   <button @click="toggleStatus">Toggle Status</button>
 
+  <form v-on:submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask">
+    <button type="submit">Submit</button>
+  </form>
 
 
   <h4>Tasks</h4>
   <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>{{ task }}</span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
   </ul>
 
-  <a :href="link">Click to visit google</a>
 </template>
 
 <style scoped>
