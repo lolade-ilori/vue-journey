06db82ddf65b1567511aef8bcbc085556150f8a6
Lolade Ilori <loladeilori02@gmail.com>
2025-02-15 13:59:51 +0000

    adding active links

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,11 @@
 <script setup>
 import logo from '@/assets/img/logo.png'
-import { RouterLink } from 'vue-router';
+import { RouterLink, useRoute } from 'vue-router';
+
+const isActive = (routePath) => {
+  const route =  useRoute()
+  return route.path === routePath
+}
 </script>
 
 <template>
@@ -21,17 +26,17 @@ import { RouterLink } from 'vue-router';
               <div class="flex space-x-2">
                 <RouterLink 
                   to="/"
-                  class="text-white bg-green-900 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  :class="[ isActive('/') ? 'bg-green-900': 'hover:bg-gray-900 hover:text-white', 'py-2', 'px-3', 'rounded-md', 'text-white',]"
                   >Home</RouterLink 
                 >
                 <RouterLink 
                   to="/jobs"
-                  class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
+                  :class="[ isActive('/jobs') ? 'bg-green-900': 'hover:bg-gray-900 hover:text-white', 'py-2', 'px-3', 'rounded-md', 'text-white',]"
                   >Jobs</RouterLink 
                 >
-                <RouterLink
+                <RouterLink 
                   to="/jobs/add"
-                  class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
+                  :class="[ isActive('/jobs/add') ? 'bg-green-900': 'hover:bg-gray-900 hover:text-white', 'py-2', 'px-3', 'rounded-md', 'text-white',]"
                   >Add Job</RouterLink 
                 >
               </div>
